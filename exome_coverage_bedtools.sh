ee8d5bcb18082620f3b628a6babe1df1a45a4909
James <jchap14@gmail.com>
2018-07-31 15:52:49 -0400

    memory increase

--- exome_coverage_bedtools.sh
+++ exome_coverage_bedtools.sh
@@ -21,7 +21,7 @@ cat > $NAME.tempscript.sh << EOF
 #SBATCH --mail-type=ALL
 # Request run time & memory
 #SBATCH --time=5:00:00
-#SBATCH --mem=4G
+#SBATCH --mem=6G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
 #SBATCH --export=ALL
