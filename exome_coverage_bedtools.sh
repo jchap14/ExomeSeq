653c6342d27abc919c900f6d7338ee2a0ce25fee
James <jchap14@gmail.com>
2018-07-31 12:58:22 -0400

    SLURM update

--- exome_coverage_bedtools.sh
+++ exome_coverage_bedtools.sh
@@ -1,6 +1,7 @@
 ##### Check breadth and depth of coverage for exome seq
 
 # for x in `/bin/ls *.markdup.realigned.bam` ; do bash exome_coverage_bedtools.sh $x; done
+# for x in `/bin/ls *.sorted.nodup.bam` ; do bash exome_coverage_bedtools.sh $x; done
 
 ## add modules
 module add bedtools/2.16.2
@@ -8,26 +9,33 @@ module add bedtools/2.16.2
 ## define variables
 BAMFILE=$1
 TARGETBED="/srv/gsfs0/projects/snyder/chappell/Annotations/clinical_exome_targets.bed"
-NAME=`basename $1 .markdup.realigned.bam`
+#NAME=`basename $1 .markdup.realigned.bam`
+NAME=`basename $1 .bwa.sorted.nodup.bam`
+
 
 cat > /tmp/tempscript.sh << EOF
-#!/bin/bash
-#$ -N $NAME.coverage
-#$ -j y
-#$ -cwd
-#$ -V
-#$ -l h_vmem=6G
-#$ -pe shm 4
-#$ -l h_rt=11:59:00
-#$ -l s_rt=11:59:00
+#!/bin/bash -l
+#SBATCH --job-name $NAME.coverage
+#SBATCH --output=$NAME.coverage.out
+#SBATCH --mail-user jchap14@stanford.edu
+#SBATCH --mail-type=ALL
+# Request run time & memory
+#SBATCH --time=5:00:00
+#SBATCH --mem=2G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --export=ALL
+#SBATCH --account=mpsnyder
 
-## run commands
+##### Run commands:
 ##usage: coverageBed [OPTIONS] -a <BED/GFF/VCF> -b <BED/GFF/VCF>
 bedtools coverage -hist -abam $BAMFILE -b $TARGETBED | grep ^all > $NAME.bam.hist.all.txt
 EOF
-qsub /tmp/tempscript.sh
+
+## qsub then remove the tempscript
+sbatch $NAME.tempscript.sh #scg
 sleep 1
-rm /tmp/tempscript.sh
+rm $NAME.tempscript.sh
 
 ##### test command: use bedtools version 2.16.2
 # bamfile="M4.trim.R1.markdup.realigned.bam"
